7911899b20fe210ca8fe148ff5bab28c74b2821a
Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
2015-07-29 13:03:30 -0400

    ubifs: implement ubifs_get_qsize to get quota size in ubifs
    We only care the size of regular file in ubifs for quota.
    The reason is similar with the comment in ubifs_getattr().
    
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>

--- fs/ubifs/ubifs.h
+++ fs/ubifs/ubifs.h
@@ -1760,6 +1760,7 @@ int ubifs_setattr(struct dentry *dentry, struct iattr *attr);
 #ifdef CONFIG_UBIFS_ATIME_SUPPORT
 int ubifs_update_time(struct inode *inode, struct timespec *time, int flags);
 #endif
+ssize_t ubifs_get_qsize(struct inode *inode);
 
 /* dir.c */
 struct inode *ubifs_new_inode(struct ubifs_info *c, const struct inode *dir,
